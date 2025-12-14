09c1cf07b54fbf0291c6c04bfeeab145d253a1b6
yavaka <yavakasolutions@gmail.com>
2025-12-14 17:08:41 +0000

    Initial Commit

---
+++ ShireBudgeters/Configurations/WebAppConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Radzen;
+
+namespace ShireBudgeters.Configurations;
+
+public static class WebAppConfigurations
+{
+    public static IServiceCollection AddWebAppServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add services to the container.
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        services.AddRadzenComponents();
+
+        // Add your web app services here
+        return services;
+    }
+}
