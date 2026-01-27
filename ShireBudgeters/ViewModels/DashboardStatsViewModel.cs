5859723fbe3d094162b8e67214bd269cd09a178a
yavaka <yavakasolutions@gmail.com>
2026-01-27 22:57:54 +0000

    SHIR-55 Implemented Admin Dashboard

---
+++ ShireBudgeters/ViewModels/DashboardStatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ShireBudgeters.ViewModels;
+
+public class DashboardStatsViewModel
+{
+    public int TotalCategories { get; set; }
+    public int ActiveCategories { get; set; }
+    public int InactiveCategories { get; set; }
+    public int TotalPosts { get; set; }
+    public int PublishedPosts { get; set; }
+    public int DraftPosts { get; set; }
+    public int TotalLeadMagnets { get; set; }
+    public int ActiveLeadMagnets { get; set; }
+    public int InactiveLeadMagnets { get; set; }
+}
